67fbc31ea6c04d526eac60cfe4861c13980f6d3d
Guillermo Apoj <guillermo.apoj@rappi.com>
2024-05-02 19:54:21 -0300

    Xcode project creation

---
+++ VideoPlayerChallenge/VideoPlayerChallenge/VideoPlayerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerChallengeApp.swift
+//  VideoPlayerChallenge
+//
+//  Created by Guillermo Apoj on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
