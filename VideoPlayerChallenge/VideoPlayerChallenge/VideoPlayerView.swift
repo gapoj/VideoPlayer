00ee7524b15a1c662d28bf32c93701a12b983ec2
gapoj <gapoj@users.noreply.github.com>
2024-05-03 00:38:40 -0300

    pause on shake as required

--- VideoPlayerChallenge/VideoPlayerChallenge/VideoPlayerView.swift
+++ VideoPlayerChallenge/VideoPlayerChallenge/VideoPlayerView.swift
@@ -11,10 +11,17 @@ import AVKit
 struct VideoPlayerView: View {
     let url = URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4")
     var body: some View {
-        if let url {
-            let videoplayer = AVPlayer(url:url)
-            VideoPlayer(player: videoplayer ).onAppear {
-                videoplayer.play()
+        VStack {
+            if let url {
+                let videoplayer = AVPlayer(url:url)
+                VideoPlayer(player: videoplayer ).onAppear {
+                    locationManager.requestLocation()
+                    videoplayer.play()
+                }.onDisappear {
+                    videoplayer.pause()
+                }.onShake { // pause on shake as required
+                    videoplayer.pause()
+                }
             }
         }
     }
@@ -22,4 +29,37 @@ struct VideoPlayerView: View {
 
 #Preview {
     VideoPlayerView()
+}
+// The notification we'll send when a shake gesture happens.
+extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
+
+//  Override the default behavior of shake gestures to send our notification instead.
+extension UIWindow {
+     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
+        }
+     }
+}
+
+// A view modifier that detects shaking and calls a function of our choosing.
+struct DeviceShakeViewModifier: ViewModifier {
+    let action: () -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.deviceDidShakeNotification)) { _ in
+                action()
+            }
+    }
+}
+
+// A View extension to make the modifier easier to use.
+extension View {
+    func onShake(perform action: @escaping () -> Void) -> some View {
+        self.modifier(DeviceShakeViewModifier(action: action))
+    }
 }
